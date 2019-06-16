c2f41afa9fcf26d0663ef2cfe53eb6b2536357ff
robertmadril <robertmmadril@gmail.com>
2019-06-16 12:58:34 -0700

    added mongoose connection to server file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +14,10 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
+);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
