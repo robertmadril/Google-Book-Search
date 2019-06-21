6a906eb593416af26e5be3425d7ffdfdf446212d
robertmadril <robertmmadril@gmail.com>
2019-06-21 13:48:42 -0700

    add results section

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,12 +1,46 @@
 import React from "react";
 import Results from "../components/Results";
 
-function Saved(props) {
-    return (
-        <div>
-            <Results/>
-        </div>
-    )
+class Saved extends Component {
+    state = {
+        books: []
+    };
+
+    componentDidMount() {
+        this.getSavedBooks();
+    }
+
+    getSavedBooks = () => {
+        API.getSavedBooks()
+            .then(res =>
+                this.setState({
+                    books: res.data
+                })
+            )
+            .catch(err => console.log(err));
+    };
+
+    handleBookDelete = id => {
+        API.deleteBook(id).then(res => this.getSavedBooks());
+    };
+
+    render() {
+        return (
+            <div>
+                {this.state.books.map(book => (
+                    <Results
+                        key={book.id}
+                        title={book.volumeInfo.title}
+                        subtitle={book.volumeInfo.subtitle}
+                        link={book.volumeInfo.infoLink}
+                        authors={book.volumeInfo.authors.join(", ")}
+                        description={book.volumeInfo.description}
+                        image={book.volumeInfo.imageLinks.thumbnail}
+                    />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default Saved;
